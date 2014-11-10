a36ded1eb73e801d636c2f9c581006e6d8f921c8
Administrator <admin@example.com>
2014-11-10 23:01:07 +0400

    Refactoring + comments

--- README.md
+++ README.md
@@ -12,22 +12,22 @@ Usage
     <script type="text/javascript" src="microuploader.js"></script>
 ```
 
- - Call **createUploader** with parameters
+ - Call **microUploader.attach** with parameters
 
 ```javascript
-    var params = {
-        elementId: 'your_element_id',
+    var opts = {
+        element: document.getElementById('your_element_id'),
         url: 'http://your_url.com/uploadLink/',
-        onBeforeSend: function() { 
-            console.log('before callback'); 
+        beforeCb: function() {
+            console.log('before callback');
         },
-        onComplete: function(data) { 
-            console.log('done callback: '+data); 
+        doneCb: function(data) {
+            console.log('done callback: '+data);
         },
-        onError: function(error) { 
-            console.log(error); 
+        errorCb: function(error) {
+            console.log(error);
         }
-    }
-    
-    createUploader(params);
+    };
+
+    microUploader.attach(opts);
 ```
--- microuploader.js
+++ microuploader.js
@@ -1,79 +1,111 @@
-uploader = function() {} 
+/**
+ * Small Javascript Ajax file uploader.
+ * @author Babykin Vadim <creativelink@ya.ru>
+ */
+var microUploader = (function(uploader) {
+    'use strict';
 
-uploader.prototype.setAttributes = function(el, attrs) {
-    for(var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
-}
-
-uploader.prototype.createFrame = function(uid) {
-   var iframe = document.createElement('iframe');
-   this.setAttributes(iframe, { "name": uid, "style": "display:none;" })
-   return iframe;
-}
-
-uploader.prototype.createFileInput = function() {
-   var fileInput =  document.createElement('input');
-   this.setAttributes(fileInput, { "name": "file[]", "type": "file", "multiple": true });
-   return fileInput;
-}
-
-uploader.prototype.createForm = function(uid, url) {
-   var form = document.createElement('form');
-   this.setAttributes(form, { "action": url, "method": "POST", "enctype": "multipart/form-data", "target": uid });
-   return form;
-}
-
-uploader.prototype.createSubmitButton = function() {
-   var submit = document.createElement('input');  
-   submit.type = "submit";
-   return submit;
-}
-
-uploader.prototype.setUploader = function (elem, url, beforeCallback, doneCallback, errorCallback) {
-    var cbBefore = typeof(beforeCallback) === "function" ? beforeCallback : function() {};
-    var cbDone = typeof(doneCallback) === "function" ? doneCallback : function() {};
-    var cbError = typeof(errorCallback) === "function" ? errorCallback : function() {};
-    var uid = (new Date().getTime()).toString(16);
-    // Создаем фрейм
-    var iframe = this.createFrame(uid); 
-    // Создаем файловый инпут
-    var fileInput =  this.createFileInput();
-    // Создаем кнопку сабмита
-    var submit = this.createSubmitButton();
-    // Создаем форму и прявязываем к ней элементы
-    var form = this.createForm(uid, url);
-    form.appendChild(fileInput).appendChild(submit);
-    // Помещаем форму в iframe
-    iframe.appendChild(form);
-    // добавляем iframe к элементу
-    elem.appendChild(iframe);
-    
-    // Привязываем события
-    elem.addEventListener("click", function() { fileInput.click(); }, true);
-    fileInput.addEventListener("change", function () { 
-        cbBefore(); 
-        form.submit(); 
-    }, true);
-
-    iframe.addEventListener("load", function () {
-        fileInput.value = '';
-        var iframeContents = '';
-        try {
-            iframeContents = this.contentWindow.document.body.innerHTML;
-        } catch(e) {
-            cbError(e);
+    /**
+     * Help function. Fast set attributes for element
+     * @param el - element
+     * @param attrs - attributes object
+     */
+    var setAttributes = function(el, attrs) {
+        for(var key in attrs) {
+            el.setAttribute(key, attrs[key]);
         }
-        cbDone([iframeContents]);      
-    }, true);
-}
+    };
 
-createUploader = function(p) {
-    p.elementId = document.getElementById(p.elementId);
-    if (p.elementId === "undefined")
-        throw('Element not found!');
-    if (p.url === "undefined" || p.url == "")
-        throw('Url is not valid!');
-    var _uploader = new uploader();
-    _uploader.setUploader(p.elementId, p.url, p.onBeforeSend, p.onComplete, p.onError);
-}
+    /**
+     * Function for creating hidden IFRAME
+     * @param uid - iframe unique id
+     * @returns {HTMLElement} - new iframe element
+     */
+    var createFrame = function(uid) {
+        var iframe = document.createElement('iframe');
+        setAttributes(iframe, { "name": uid, "style": "display:none;" });
+        return iframe;
+    };
+
+    /**
+     * Creating hidden file input
+     * @returns {HTMLElement} - new file input element
+     */
+    var createFileInput = function() {
+        var fileInput =  document.createElement('input');
+        setAttributes(fileInput, { "name": "file[]", "type": "file", "multiple": true });
+        return fileInput;
+    };
+
+    /**
+     * Creating hidden form with multipart content
+     * @param uid - form unique id
+     * @param url - url for POST-data
+     * @returns {HTMLElement} - new FORM element
+     */
+    var createForm = function(uid, url) {
+        var form = document.createElement('form');
+        setAttributes(form, {
+            "action": url,
+            "method": "POST",
+            "enctype": "multipart/form-data",
+            "target": uid
+        });
+        return form;
+    };
+
+    /**
+     * Creating hidden submit button element
+     * @returns {HTMLElement} - submit element
+     */
+    var createSubmitButton = function() {
+        var submit = document.createElement('input');
+        submit.type = "submit";
+        return submit;
+    };
+
+    /**
+     * Binding function
+     * @param params
+     */
+    uploader.attach = function(params) {
+        var elem = params.element,
+            url = params.url;
+        var cbBefore = typeof(params.beforeCb) === "function" ? params.beforeCb : function() {};
+        var cbDone = typeof(params.doneCb) === "function" ? params.doneCb : function() {};
+        var cbError = typeof(params.errorCb) === "function" ? params.errorCb : function() {};
+        var uid = (new Date().getTime()).toString(16);
+        // Creating frame
+        var iframe = createFrame(uid);
+        // Creating new input field
+        var fileInput =  createFileInput();
+        // Creating submit button
+        var submit = createSubmitButton();
+        //Creating form and binding elements
+        var form = createForm(uid, url);
+        form.appendChild(fileInput).appendChild(submit);
+        // Attach form to iframe
+        iframe.appendChild(form);
+        // Attach iframe to element
+        elem.appendChild(iframe);
+
+        // Bind events
+        elem.addEventListener("click", function() { fileInput.click(); }, true);
+        fileInput.addEventListener("change", function () {
+            cbBefore();
+            form.submit();
+        }, true);
+
+        iframe.addEventListener("load", function () {
+            fileInput.value = '';
+            var iframeContents = '';
+            try {
+                iframeContents = this.contentWindow.document.body.innerHTML;
+            } catch(e) {
+                cbError(e);
+            }
+            cbDone([iframeContents]);
+        }, true);
+    };
+    return uploader;
+})(microUploader || {});
